2989e6808b0148411f71a7de107750059810534c
Ben Holmes <mail@benholmes.uk>
2015-08-17 22:04:46 -0700

    Configure the accessToken through the config file

--- index.js
+++ index.js
@@ -7,4 +7,23 @@ module.exports = {
   included: function included(app) {
     app.import(app.bowerDirectory + '/mapbox.js/mapbox.js');
   },
+
+  contentFor: function(type, config) {
+    var content = [];
+
+    if (type === 'body-footer') {
+      if (!config.mapbox || !config.mapbox.accessToken) {
+        console.error('Please specify your mapbox.accessToken in your config.');
+        return content;
+      }
+
+      content = [
+        '<script>',
+        'L.mapbox.accessToken = "' + config.mapbox.accessToken + '";',
+        '</script>',
+      ];
+    }
+
+    return content;
+  },
 };
