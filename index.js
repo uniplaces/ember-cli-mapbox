d66c3ec74af2715021bc0bfee7258f50bb865568
Ben Holmes <mail@benholmes.uk>
2015-09-09 15:22:05 -0700

    Add images assets to build

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
   name: 'ember-cli-mapbox',
 
   included: function included(app) {
     app.import(app.bowerDirectory + '/mapbox.js/mapbox.js');
     app.import(app.bowerDirectory + '/mapbox.js/mapbox.css');
+  },
+
+  treeForPublic: function() {
+    var mapboxPath = path.join(this.app.bowerDirectory, 'mapbox.js');
+    var mapboxTree = new Funnel(this.treeGenerator(mapboxPath), {
+      srcDir: '/images',
+      destDir: '/assets/images',
+    });
+
+    return mapboxTree;
   },
 
   contentFor: function(type, config) {
