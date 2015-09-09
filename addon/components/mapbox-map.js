5f36378a639f48478d4ff6a956f5167f4b07693f
Ben Holmes <mail@benholmes.uk>
2015-09-09 13:49:57 -0700

    Specify a div id for the map instance

--- addon/components/mapbox-map.js
+++ addon/components/mapbox-map.js
@@ -3,11 +3,11 @@ import layout from '../templates/components/mapbox-map';
 
 export default Ember.Component.extend({
   layout: layout,
+  divId: 'map',
 
   mapId: null,
-  id: 'map',
 
   setup: Ember.on('didInsertElement', function() {
-    L.mapbox.map('map', this.get('mapId'));
+    L.mapbox.map(this.get('divId'), this.get('mapId'));
   }),
 });
