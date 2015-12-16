e7678528efd7b0cb51c0563a958dd525a65bd09c
Martin Kautz <kautz@ocean-insights.com>
2015-12-16 16:51:57 +0100

    Changed the way to get the config object during init yet again.

--- addon/initializers/mapbox-access-code.js
+++ addon/initializers/mapbox-access-code.js
@@ -1,11 +1,13 @@
 export function initialize(instance) {
   let container = this.container;
-  if (!container) {
+  if (!container && instance.container) {
     container = instance.container();
+  } else {
+    container = instance.__container__;
   }
 
   let config = container.lookupFactory('config:environment');
-
+  
   if (!config.mapbox || !config.mapbox.accessToken) {
     console.error('Please specify your mapbox.accessToken in your config.');
     return;
