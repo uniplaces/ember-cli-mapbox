236cfdf6686156c0b23c18cc2c9a407dcbf0282f
Wim Pool <wim.pool@ihomer.nl>
2016-02-11 17:19:49 +0100

    Make marker icon reactive
    replace the marker icon when color, size or symbol changes

--- addon/components/mapbox-marker.js
+++ addon/components/mapbox-marker.js
@@ -18,6 +18,18 @@ export default Ember.Component.extend({
       return false;
     }
   }),
+  
+  iconChange: Ember.observer('color', 'size', 'symbol', function() {
+    var map = this.get('map');
+    var marker = this.get('marker');
+    if (typeof(map) != 'undefined' && marker != null) {
+      marker.setIcon(L.mapbox.marker.icon({
+        'marker-color': this.get('color'),
+        'marker-size': this.get('size'),
+        'marker-symbol': this.get('symbol')
+      }));
+    }
+  }),
 
   setup: Ember.on('init', function() {
     let marker = L.marker(this.get('coordinates'), {
