531ae0d90cad9df26366266ad49a4a2497add6c8
Miles Zimmerman <miles@maztek.io>
2016-02-11 10:48:32 -0800

    Removing post-install bower and updating to mapbox 2.3.0

--- blueprints/ember-cli-mapbox/index.js
+++ blueprints/ember-cli-mapbox/index.js
@@ -6,10 +6,6 @@ module.exports = {
   },
 
   afterInstall: function() {
-    // we need to install moment-timezone via bower since npmignore
-    // ignores `moment-timezone/builds/*`
-    return this.addBowerPackagesToProject([
-      { name: 'mapbox.js', target: '>= 2.2.1' },
-    ]);
+    // nothing
   },
 };
