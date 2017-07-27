84d549607d3a908f5f73a73bdc5e46dbdeda5aec
Paulo Monteiro <paulomonteiro7.pm@gmail.com>
2017-07-27 15:58:18 +0100

    Allow to add options to mapbox map

--- addon/components/mapbox-map.js
+++ addon/components/mapbox-map.js
@@ -5,35 +5,37 @@ import { MAP_EVENTS } from '../constants/events';
 export default Ember.Component.extend({
   layout: layout,
   divId: 'map',
-
   mapId: null,
 
-  setup: Ember.on('didInsertElement', function() {
+  // Map Options
+  center: null,
+  zoom: null,
+  style: 'mapbox://styles/mapbox/basic-v9',
+  dragging: true,
+  scrollWheelZoom: true,
+  doubleClickZoom: true,
+  tap: true,
+  options: {},
+
+  didInsertElement() {
     Ember.run.scheduleOnce('afterRender', this, function() {
-      let map = L.mapbox.map(this.get('divId'), this.get('mapId'));
+      let map = L.mapbox.map(
+        this.get('divId'),
+        this.get('mapId'),
+        {
+          ...this.getProperties('center', 'zoom', 'dragging', 'scrollWheelZoom', 'doubleClickZoom', 'tap'),
+          ...this.get('options')
+        }
+      );
+
+      L.mapbox.styleLayer(this.get('style')).addTo(map);
 
       // Bind Events
       MAP_EVENTS.forEach((event) => {
         map.on(event, (e) => this.sendAction('on' + event, map, e));
       });
 
-      // Setters
-      if (this.get('center')) {
-        map.setView(this.get('center'), this.get('zoom'));
-      }
-
-      if (this.get('click')) {
-        Ember.deprecate('The "click" action in mapbox-map is deprecated, please use "onclick" instead.', false, {
-          id: 'mapbox-map-click-action',
-          url: 'https://github.com/binhums/ember-cli-mapbox',
-          until: '1 April 2016'
-        });
-
-        map.on('click', (e) => this.sendAction('click', map, e));
-      }
-
-      // Set
       this.set('map', map);
     });
-  })
+  },
 });
--- addon/components/mapbox-marker.js
+++ addon/components/mapbox-marker.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import layout from '../templates/components/mapbox-marker';
 import { MARKER_EVENTS } from '../constants/events';
 
+const { on, computed, isEmpty, observer } = Ember;
+
 export default Ember.Component.extend({
   classNameBindings: ['isLoaded'],
   layout: layout,
@@ -9,68 +11,76 @@ export default Ember.Component.extend({
   color: '#444444',
   marker: null,
   draggable: false,
+  hasEvents: true,
 
-  isLoaded: Ember.computed('map', 'marker', function() {
-    let map = this.get('map');
-    let marker = this.get('marker');
-    let cluster = this.get('cluster');
+  createMarkerIcon() {
+    return L.mapbox.marker.icon({
+      'marker-color': this.get('color'),
+      'marker-size': this.get('size'),
+      'marker-symbol': this.get('symbol')
+    });
+  },
 
-    if (!Ember.isEmpty(map) && !Ember.isEmpty(marker)) {
-      if (!Ember.isEmpty(cluster)) {
-        cluster.addLayer(marker);
-      } else {
-        marker.addTo(map);
-      }
-      return true;
-    } else {
+  isLoaded: computed('map', 'marker', function() {
+    let {map, marker, cluster } = this.getProperties('map', 'marker', 'cluster');
+
+    if (isEmpty(map) || isEmpty(marker)) {
       return false;
     }
+
+    if (!isEmpty(cluster)) {
+      cluster.addLayer(marker);
+    } else {
+      marker.addTo(map);
+    }
+
+    return true;
   }),
 
-  iconChange: Ember.observer('color', 'size', 'symbol', function() {
-    let map = this.get('map');
-    let marker = this.get('marker');
+  iconChange: observer('color', 'size', 'symbol', function() {
+    let { map, marker } = this.getProperties('map', 'marker');
+
     if (typeof map !== 'undefined' && marker != null) {
-      marker.setIcon(L.mapbox.marker.icon({
-        'marker-color': this.get('color'),
-        'marker-size': this.get('size'),
-        'marker-symbol': this.get('symbol')
-      }));
+      marker.setIcon(this.get('createMarkerIcon')());
     }
   }),
 
-  setup: Ember.on('init', function() {
+  setup: on('init', function() {
     let marker = L.marker(this.get('coordinates'), {
-      icon: L.mapbox.marker.icon({
-        'marker-color': this.get('color'),
-        'marker-size': this.get('size'),
-        'marker-symbol': this.get('symbol')
-      }),
+      icon: this.get('createMarkerIcon')(),
       draggable: this.get('draggable')
     });
-    marker.bindPopup(this.get('popup-title'));
 
-    MARKER_EVENTS.forEach((event) => {
-      marker.on(event, (e) => this.sendAction('on' + event, marker, e));
-    });
+    if (this.get('hasEvents')) {
+      marker.bindPopup(this.get('popup-title'));
+
+      MARKER_EVENTS.forEach((event) => {
+        marker.on(event, (e) => this.sendAction('on' + event, marker, e));
+      });
+    }
 
     this.set('marker', marker);
   }),
 
-  teardown: Ember.on('willDestroyElement', function() {
-    let marker = this.get('marker');
-    let map = this.get('map');
+  teardown: on('willDestroyElement', function() {
+    let { map, marker } = this.getProperties('map', 'marker');
+    
     if (map && marker) {
       map.removeLayer(marker);
     }
   }),
 
-  popup: Ember.on('didRender', function() {
-    if (this.get('is-open')) {
+  popup: on('didRender', function() {
+    if (!this.get('is-open')) {
+      return;
+    }
+
+    if (this.get('hasEvents')) {
       this.get('marker').openPopup();
-      if (this.get('recenter')) {
-        this.get('map').setView(this.get('coordinates'));
-      }
+    }
+
+    if (this.get('recenter')) {
+      this.get('map').setView(this.get('coordinates'));
     }
   })
 });
