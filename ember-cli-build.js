1b5661a90f76a0906eba9971ec3bb631b0d81a4f
Alexandre Santos <alexandre.santos@uniplaces.com>
2017-12-05 11:49:54 +0000

    Add spread operator transform

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,11 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+     options: {
+      babel: {
+        plugins: ['transform-object-rest-spread']
+      }
+    }
   });
 
   return app.toTree();
